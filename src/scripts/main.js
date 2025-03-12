a07d5f4cf4d2cf98573e14bf3352c85bda7fad46
adrianasheila <adrisheiaquiblan@gmail.com>
2025-03-12 17:46:59 -0400

    mejora1miniproyecto

---
+++ src/scripts/main.js
@@ -0,0 +1,60 @@
+const imagenlogos = document.querySelector("#imagenlogo")
+const habilidadeslogos = [
+    {
+        image: "./src/imagen /html.png",
+        description:"logo html",
+
+    },
+    {
+        image: "./ src / imagen / html.png",
+        description:"logo html",
+
+    },
+    {
+        image: "./ src / imagen / html.png",
+        description:"logo html",
+
+    },
+    {
+        image: "./ src / imagen / html.png",
+        description:"logo html",
+
+    },
+    {
+        image: "./ src / imagen / html.png",
+        description:"logo html",
+
+    },
+    {
+        image: "./ src / imagen / html.png",
+        description:"logo html",
+
+    },
+    {
+        image: "./ src / imagen / html.png",
+        description:"logo html",
+
+    },
+    {
+        image: "./ src / imagen / html.png",
+        description:"logo html",
+
+    },
+    {
+        image: "./ src / imagen / html.png",
+        description:"logo html",
+
+    },
+    
+
+]
+
+function ordenar(items, elementos) {
+
+    for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+        const template = ``
+
+    }
+}
+ordenar(habilidadeslogos, imagenlogos)
