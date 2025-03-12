3856878200891fcbc86a8cc3a9f3eb60908406aa
adrianasheila <adrisheiaquiblan@gmail.com>
2025-03-12 19:19:46 -0400

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,58 +1,51 @@
 const imagenlogos = document.querySelector("#imagenlogo")
 const habilidadeslogos = [
     {
-        image: "./src/imagen /html.png",
-        description:"logo html",
+        image: "./src/imagen/html.png",
+        description: "logo html",
 
     },
     {
-        image: "./ src / imagen / html.png",
-        description:"logo html",
+        image: "./src/imagen/tailwind.png",
+        description: "logo html",
 
     },
     {
-        image: "./ src / imagen / html.png",
-        description:"logo html",
+        image: "./src/imagen/css.png",
+        description: "logo html",
 
     },
     {
-        image: "./ src / imagen / html.png",
-        description:"logo html",
+        image: "./src/imagen/ts.png",
+        description: "logo html",
 
     },
     {
-        image: "./ src / imagen / html.png",
-        description:"logo html",
+        image: "./src/imagen/javas.png",
+        description: "logo html",
 
     },
     {
-        image: "./ src / imagen / html.png",
-        description:"logo html",
+        image: "./src/imagen/next.png",
+        description: "logo html",
 
     },
     {
-        image: "./ src / imagen / html.png",
-        description:"logo html",
+        image: "./src/imagen/hola.png",
+        description: "logo html",
 
     },
     {
-        image: "./ src / imagen / html.png",
-        description:"logo html",
+        image: "./src/imagen/jason.png",
+        description: "logo html",
 
     },
-    {
-        image: "./ src / imagen / html.png",
-        description:"logo html",
-
-    },
-    
-
 ]
 
-function ordenar(items, elementos) {
+function ordenar(datos, indexhtml) {
 
-    for (let i = 0; i < items.length; i++) {
-        const item = items[i];
+    for (let i = 0; i < datos.length; i++) {
+        const item = datos[i];
         const template = ``
 
     }
