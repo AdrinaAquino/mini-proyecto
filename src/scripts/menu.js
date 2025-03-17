5e6f04dd75883ec4881fd88281c7c7e7063bb6a9
adrianasheila <adrisheiaquiblan@gmail.com>
2025-03-17 05:17:18 -0400

    Miniproyecto2

--- src/scripts/menu.js
+++
@@ -1,9 +0,0 @@
-const menu = document.querySelector('#menu');
-const navBar = document.querySelector('#opciones');
-
-
-menu.addEventListener("click", function () {
-    console.log("hola mun");
-    navBar.classList.toggle("opciones");
-    
-})
